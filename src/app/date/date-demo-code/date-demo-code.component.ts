1cfc5f72d52df05f6149e0f50cc54cd9cd986609
Johan Aspeling <johan.aspeling@gmail.com>
2025-01-03 16:26:56 +0100

    chore: updated to v19

--- src/app/date/date-demo-code/date-demo-code.component.ts
+++ src/app/date/date-demo-code/date-demo-code.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-date-demo-code',
   templateUrl: './date-demo-code.component.html',
+  standalone: false,
 })
 export class DateDemoCodeComponent implements OnInit {
   constructor() {}
--- src/app/numeric/numeric-demo-code/numeric-demo-code.component.ts
+++ src/app/numeric/numeric-demo-code/numeric-demo-code.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-numeric-demo-code',
   templateUrl: './numeric-demo-code.component.html',
+  standalone: false,
 })
 export class NumericDemoCodeComponent implements OnInit {
   constructor() {}
